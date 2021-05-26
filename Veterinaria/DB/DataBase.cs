0c1478057b540208d1af5d79e7e5e5f6c46739c5
deadchri5 <deadchri5h@gmail.com>
2021-05-26 02:24:21 -0500

    Registro hecho

---
+++ Veterinaria/DB/DataBase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace Veterinaria.DB
+{
+    class DataBase
+    {
+        private string server;
+        private string bd;
+        private string user;
+        private string connectionString;
+        private string datos;
+
+        public DataBase(string server, string bd, string user)
+        {
+            this.server = server;
+            this.bd = bd;
+            this.user = user;
+            this.connectionString = $"Database={bd}; Data Source={server}; User Id={user}; Password=;";
+        }
+
+        private void OpenConnection(MySqlConnection con)
+        {
+            try
+            {
+                con.Open();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void CloseConnection(MySqlConnection con)
+        {
+            try
+            {
+                con.Close();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void executeQuery(string query)
+        {
+            MySqlConnection connectionDB = new MySqlConnection(connectionString);
+            OpenConnection(connectionDB);
+            try
+            {
+                MySqlCommand command = new MySqlCommand(query);
+                command.Connection = connectionDB;
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    datos += reader.GetString(0) + "\n";
+                }
+                if (datos != null)
+                    MessageBox.Show(datos);
+                CloseConnection(connectionDB);
+            }
+            catch (MySqlException e) 
+            {
+                MessageBox.Show(e.Message, "Error en consulta.", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+    }
+}
--- Veterinaria/Views/UserRegisterView.xaml.cs
+++ Veterinaria/Views/UserRegisterView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Veterinaria.DB;
 
 namespace Veterinaria.Views
 {
@@ -24,6 +26,8 @@ namespace Veterinaria.Views
         {
             InitializeComponent();
         }
+
+        DataBase db = new DataBase("localhost", "pet_control", "root");
 
         private void OpenLoginView_Click(object sender, RoutedEventArgs e)
         {
@@ -40,6 +44,45 @@ namespace Veterinaria.Views
         private void MinimizeWindow_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            CanvasLoading.Visibility = Visibility.Visible;
+            string name = TextBoxName.Text;
+            string apellido = TextBoxApellido.Text;
+            string email = TextBoxEmail.Text;
+            string password = TextBoxPassword.Password;
+
+            if (name.Length > 0 && apellido.Length > 0 && password.Length > 0)
+            {
+                if (IsValidEmailAddress(email))
+                {
+                    db.executeQuery("INSERT INTO cliente(Nombre, Apellidos, Email, Password, FK_tipo) " +
+                            $"VALUES ('{name}', '{apellido}', '{email}', '{password}', '2')");
+                    CanvasLoading.Visibility = Visibility.Hidden;
+                    CanvasDone.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageBox.Show("Ingresa una dirección de correo eléctronico en formato correcto.",
+                                        "Error en registro", MessageBoxButton.OK, MessageBoxImage.Error);
+                    CanvasLoading.Visibility = Visibility.Hidden;
+                }
+                    
+            }
+            else
+            {
+                MessageBox.Show("Porfavor llena todos los campos del formulario.",
+                    "Error en registro", MessageBoxButton.OK, MessageBoxImage.Error);
+                CanvasLoading.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private bool IsValidEmailAddress(string email)
+        {
+            Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            return regex.IsMatch(email);
         }
     }
 }
