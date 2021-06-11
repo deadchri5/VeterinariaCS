fdb6d14c3eafbacb53ee53587a5af34ede73b446
Mariovmv <mario00375@gmail.com>
2021-06-11 00:42:34 -0500

    vista de doctor

---
+++ Doctor/Views/DataDoc.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Doctor.Views
+{
+    /// <summary>
+    /// Lógica de interacción para DataDoc.xaml
+    /// </summary>
+    public partial class DataDoc : Page
+    {
+        public DataDoc()
+        {
+            InitializeComponent();
+        }
+    }
+}
